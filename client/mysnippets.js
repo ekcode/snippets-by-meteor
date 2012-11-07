136f84caac436864ffc673db0ede22693c86d9da
Ickhyun Kwon <ekcode@icloud.com>
2012-11-07 21:21:43 +0900

    if first line starts with //, it will be title

--- client/mysnippets.js
+++ client/mysnippets.js
@@ -14,11 +14,21 @@ Template.inputarea.snippets = function () {
 Template.inputarea.events({
     'click #btnInsert': function () {
         var content = $('textarea').val();
+        var title = '';
+        var lines = $('textarea').val().split('\n');
+        if(lines[0].substring(0,2) == '//') {
+            title = lines[0].substring(2);
+            lines.shift(1);
+            content = lines.join('\n');
+        }
+
         var date = new Date();
         if(!content.length) {
             return;
         }
-        Snippets.insert({content:content,
+
+        Snippets.insert({title:title,
+            content:content,
             regTimestamp:date.getTime(),
             regdate:date.toLocaleDateString(),
             user:Meteor.user()});
