3eddce2b1e5a923473aaad95bd47ceb95828ff79
Ickhyun Kwon <ekcode@icloud.com>
2012-11-16 15:58:29 +0900

    update allow for user

--- server/secure.js
+++ server/secure.js
@@ -3,12 +3,16 @@ Snippets.allow({
     insert: function(userId, doc) {
         return !!Meteor.user();
     },
-    remove: function(userId, docs) {
+    remove: function(userId, docs, fields, modifier) {
+        return _.all(docs, function(doc) {
+            return doc.user._id === userId;
+        });
+    },
+    update: function(userId, docs) {
         return _.all(docs, function(doc) {
             return doc.user._id === userId;
         });
     }
-
 });
 
 Snippets.deny({});
