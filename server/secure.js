b2661f68ad15fccaf2e5fe12dcccd9d358fa924d
Ickhyun Kwon <ekcode@icloud.com>
2013-04-03 11:59:54 +0900

    fix remove action

--- server/secure.js
+++ server/secure.js
@@ -3,16 +3,14 @@ Snippets.allow({
     insert: function(userId, doc) {
         return !!Meteor.user();
     },
-    remove: function(userId, docs, fields, modifier) {
-        return _.all(docs, function(doc) {
-            return doc.user._id === userId;
-        });
+    remove: function(userId, doc) {
+        return doc.user._id === userId;
     },
-    update: function(userId, docs) {
-        return _.all(docs, function(doc) {
-            return doc.user._id === userId;
-        });
-    }
+    //update: function(userId, docs, fields, modifier) {
+    //    return _.all(docs, function(doc) {
+    //        return doc.user._id === userId;
+    //    });
+    //}
 });
 
 Snippets.deny({});
